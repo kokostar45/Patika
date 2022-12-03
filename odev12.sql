a0f42f034ab9ce50d9d79133af60025c24939205
kokostar45 <86301854+kokostar45@users.noreply.github.com>
2022-12-04 02:16:27 +0300

    Update odev12.sql

--- odev12.sql
+++ odev12.sql
@@ -1,22 +1,22 @@
-1-film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+--1-film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
 
 select count(*) from film
-where length > (select avg(length) from film);
+where length > (select AVG(length) from film);
 
 
-2-film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+--2-film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
 
 select count(*) from film
 where rental_rate = (select max(rental_rate) from film);
 
 
-3-film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
+--3-film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
 
 select * from film
 where rental_rate = (select min(rental_rate) from film) and replacement_cost = (select min(replacement_cost) from film);
 
 
-4-payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+--4-payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
 
 select customer_id,count(*) from payment
 group by customer_id
